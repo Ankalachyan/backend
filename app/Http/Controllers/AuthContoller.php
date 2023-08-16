eae17cf7e075d48a2ad78f9da4135bc80c2933e2
ankalachyan <an.kalachyan@icloud.com>
2023-08-16 18:04:39 +0400

    final backend files

--- app/Http/Controllers/AuthContoller.php
+++ app/Http/Controllers/AuthContoller.php
@@ -11,31 +11,44 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthContoller extends Controller
 {
- public function register(RegisterRequest $request){
-    $data = $request->validated();
-    $user = User::create([
-        "name" => $data["name"],
-        "email" => $data['email'],
-        "password" => Hash::make($data["password"])
-    ]);
-    $token = $user->createToken('auth_token')->plainTextToken;
-    $cookie = cookie("token", $token, 60 * 24);
-    return response()->json([
-        "user" => new UserResource($user),
-    ])->withCookie($cookie);
- }
- public function login(LoginRequest $request){
-    $data = $request->validated;
-    $user = User::where("email",$data["email"])->first();
-    if (!$user || !Hash::check($data["password"],$user->password)) {
+    public function register(RegisterRequest $request)
+    {
+        $data = $request->validated();
+        $user = User::create([
+            "name" => $data["name"],
+            "email" => $data['email'],
+            "password" => Hash::make($data["password"])
+        ]);
+        $token = $user->createToken('auth_token')->plainTextToken;
+        $cookie = cookie("token", $token, 60 * 24);
         return response()->json([
-            "message" => "Email or password is wrong"
-        ],401);
+            "user" => new UserResource($user),
+        ])->withCookie($cookie);
     }
-    $token = $user->createToken('auth_token')->plainTextToken;
-    $cookie = cookie("token", $token , 60 * 24);
-    return response()->json([
-        $user = new UserResource($user),
-    ])->withCookie($cookie);
- }
+    public function login(LoginRequest $request)
+    {
+        $data = $request->validated;
+        $user = User::where("email", $data["email"])->first();
+        if (!$user || !Hash::check($data["password"], $user->password)) {
+            return response()->json([
+                "message" => "Email or password is wrong"
+            ], 401);
+        }
+        $token = $user->createToken('auth_token')->plainTextToken;
+        $cookie = cookie("token", $token, 60 * 24);
+        return response()->json([
+            $user = new UserResource($user),
+        ])->withCookie($cookie);
+    }
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        $cookie = cookie()->forget("token");
+        return response()->json([
+            "message" => "You've been logged out"
+        ])->withCookie($cookie);
+    }
+    public function user(Request $request){
+        return new UserResource($request->user());
+    }
 }
